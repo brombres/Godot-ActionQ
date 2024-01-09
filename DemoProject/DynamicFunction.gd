1e46911f08a47ccddea4f67705ec050994b2094b
Brom Bresenham <brom.bresenham@runegate.com>
2024-01-09 12:28:22 -0500

    [DynamicFunction] Added returns_value constructor flag

--- DemoProject/DynamicFunction.gd
+++ DemoProject/DynamicFunction.gd
@@ -3,10 +3,12 @@ class_name DynamicFunction extends RefCounted
 var arg_names:Array[String]
 var statement:String
 var object:Variant
+var returns_value:bool
 
-func _init( _arg_names:Array[String], _statement:String ):
+func _init( _arg_names:Array[String], _statement:String, _returns_value:bool=false ):
 	arg_names = _arg_names
 	statement = _statement
+	returns_value = _returns_value
 
 func execute( args:Array=[] ):
 	if not object:
@@ -16,7 +18,10 @@ func execute( args:Array=[] ):
 
 		var gdscript = GDScript.new()
 		gdscript.source_code = source
-		gdscript.source_code = "%s  %s\n" % [source,statement]
+		if returns_value:
+			gdscript.source_code = "%s  return %s\n" % [source,statement]
+		else:
+			gdscript.source_code = "%s  %s\n" % [source,statement]
 		gdscript.reload()
 
 		object = RefCounted.new()
